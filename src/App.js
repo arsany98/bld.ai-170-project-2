2a6fd62746d0a5a20b9e11ce0ffc1a5749a37d0f
arsany98 <arsanymaged13@gmail.com>
2022-08-22 02:06:20 +0200

    feat: Add Course component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Course from "./Course";
+import db from "./courses_db.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Course course={db.tracks[0].courses[0]}></Course>
     </div>
   );
 }
